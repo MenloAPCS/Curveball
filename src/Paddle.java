63ecd622165c6266e86782128f8965313e13f443
RJ <rj.babiera@menloschool.org>
2015-05-13 11:31:10 -0700

    ringo tsar

---
+++ src/Paddle.java
@@ -0,0 +1,9 @@
+public class Paddle extends Rectangle
+{
+
+	public Paddle(Position3D p1In, Position3D p2In, Position3D p3In, Position3D p4In)
+	{
+		super(p1In, p2In, p3In, p4In);
+	}
+
+}
---
+++ src/Player.java
@@ -0,0 +1,14 @@
+public class Player 
+{
+	private int lives;
+	
+	public Player()
+	{
+		lives = 4;
+	}
+	
+	public int getLives()
+	{
+		return lives;
+	}
+}
