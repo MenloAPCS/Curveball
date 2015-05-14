de300487de2f01289fd44e50845734b336931325
yohanstra <jreinstra@gmail.com>
2015-05-13 22:34:57 -0700

    Fixed rendering of sphere

--- src/Sphere.java
+++ src/Sphere.java
@@ -34,22 +34,43 @@ public class Sphere extends GameObject
 	
 	private int get2DRadius(Camera cam)
 	{
-		double distance = super.getDistance(cam);
+		System.out.println();
 		Position3D center = super.getCenter();
 		Position3D camPos = cam.getPosition();
+		double distance = super.getDistance(cam);
+		double horizDistance = Math.sqrt(
+			Math.pow(center.getZ()-camPos.getZ(), 2) + 
+			Math.pow(center.getX()-camPos.getX(), 2)
+		);
+		System.out.println("Distance:" + distance + " " + horizDistance);
 		//will need to also adjust by x/z Angle
-		double yAngle = Math.asin(
-			Math.abs(center.getY() - camPos.getY()) / distance
-		);
+		double yAngleSin = (camPos.getY() - center.getY()) / distance;
+		double yAngleCos = horizDistance / distance;
+		double zAngleSin = (camPos.getZ() - center.getZ()) / horizDistance;
+		double zAngleCos = (camPos.getX() - center.getX()) / horizDistance;
+		System.out.println(yAngleSin + " " + yAngleCos + " " + zAngleSin + " " + zAngleCos);
+		double height = radius*yAngleCos;
+		double width = radius*yAngleSin;
+		double x = width*zAngleSin;
+		double z = width*zAngleCos;
 		Position3D top = new Position3D(
-			center.getX() + radius*Math.sin(yAngle), 
-			center.getY() + radius*Math.cos(yAngle),
-			center.getZ() + radius*Math.sin(yAngle)
+			center.getX() + x, 
+			center.getY() + height,
+			center.getZ() + z
 		);
+		Position3D topRev = new Position3D(
+			center.getX() - x, 
+			center.getY() + height,
+			center.getZ() - z
+		);
+		System.out.println("Center: " + center + ", top: " + top);
 		
 		Position2D center2D = translatePoint(center, cam);
 		Position2D top2D = translatePoint(top, cam);
+		Position2D topRev2D = translatePoint(topRev, cam);
 		
-		return Math.abs(top2D.getY() - center2D.getY());
+		int result = (int)distanceBetweenPoints(top2D, center2D);
+		int resultRev = (int) distanceBetweenPoints(topRev2D, center2D);
+		return Math.max(result, resultRev);
 	}
 }
