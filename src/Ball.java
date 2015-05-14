01a8f69e6bed7d9bc9769ddcedc8c7f01e260b57
yohanstra <jreinstra@gmail.com>
2015-05-14 10:59:01 -0700

    Completed ball object with velocity & collisions

--- src/Ball.java
+++ src/Ball.java
@@ -2,19 +2,102 @@ import java.awt.Color;
 
 public class Ball extends Sphere
 {
-	int vX;
-	int vY;
-	int vZ;
+	Vector3 velocity;
+	Vector3 acceleration;
+	Vector3 lowerLeftBound;
+	Vector3 upperRightBound;
 	
-	public Ball(int radius, Position3D center)
+	public Ball(
+		double radius,
+		Vector3 center, 
+		Vector3 lowerLeftBoundIn,
+		Vector3 upperRightBoundIn, 
+		double zAccel
+	)
 	{
 		super(radius, center);
-		vX = vY = vZ = 0;
+		initialize(lowerLeftBoundIn, upperRightBoundIn, zAccel);
 	}
 	
-	public Ball(int radius, Position3D center, Color color)
+	public Ball(
+		double radius,
+		Vector3 center,
+		Vector3 lowerLeftBoundIn,
+		Vector3 upperRightBoundIn,
+		double zAccel,
+		Color color
+	)
 	{
 		super(radius, center, color);
-		vX = vY = vZ = 0;
+		initialize(lowerLeftBoundIn, upperRightBoundIn, zAccel);
+	}
+	
+	private void initialize(
+		Vector3 lowerLeftBoundIn,
+		Vector3 upperRightBoundIn,
+		double zAccel
+	)
+	{
+		velocity = new Vector3(0, 0, 0);
+		acceleration = new Vector3(0, 0, zAccel);
+		lowerLeftBound = lowerLeftBoundIn;
+		upperRightBound = upperRightBoundIn;
+	}
+	
+	public void setVelocity(Vector3 velocityIn)
+	{
+		velocity = velocityIn;
+	}
+	
+	public void setAcceleration(Vector3 accelerationIn)
+	{
+		acceleration = accelerationIn;
+	}
+	
+	public Vector3 getVelocity()
+	{
+		return velocity;
+	}
+	
+	public Vector3 getAcceleration()
+	{
+		return acceleration;
+	}
+	
+	public void step()
+	{
+		System.out.println("Step!");
+		System.out.println("c: " + super.getCenter() + ", v: " + velocity + ", a: " + acceleration);
+		velocity = velocity.add(acceleration);
+		Vector3 center = super.getCenter();
+		adjustForCollisions(center);
+		super.setCenter(center.add(velocity));
+	}
+	
+	private void adjustForCollisions(Vector3 center)
+	{
+		Vector3 nextPos = center.add(velocity);
+		double radius = super.getRadius();
+		if(
+			nextPos.getX() + radius > upperRightBound.getX() ||
+			nextPos.getX() - radius < lowerLeftBound.getX()
+		)
+		{
+			velocity = velocity.multiply(new Vector3(-1, 1, 1));
+		}
+		if(
+			nextPos.getY() + radius > upperRightBound.getY() ||
+			nextPos.getY() - radius < lowerLeftBound.getY()
+		)
+		{
+			velocity = velocity.multiply(new Vector3(1, -1, 1));
+		}
+		if(
+			nextPos.getZ() + radius > upperRightBound.getZ() ||
+			nextPos.getZ() - radius < lowerLeftBound.getZ()
+		)
+		{
+			velocity = velocity.multiply(new Vector3(1, 1, -1));
+		}
 	}
 }
