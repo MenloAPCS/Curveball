8939d0b8059af7c01b967ffac60e3f23b4b54bf5
yohanstra <jreinstra@gmail.com>
2015-05-14 10:58:42 -0700

    Made radius a double

--- src/Sphere.java
+++ src/Sphere.java
@@ -7,13 +7,13 @@ public class Sphere extends GameObject
 	
 	private double radius;
 	
-	public Sphere(int radiusIn, Position3D center)
+	public Sphere(double radiusIn, Vector3 center)
 	{
 		super(center);
 		radius = radiusIn;
 	}
 	
-	public Sphere(int radiusIn, Position3D center, Color color)
+	public Sphere(double radiusIn, Vector3 center, Color color)
 	{
 		super(center, color);
 		radius = radiusIn;
@@ -28,46 +28,46 @@ public class Sphere extends GameObject
 	{
 		super.render(g, cam);
 		int radius2D = get2DRadius(cam);
-		Position2D center2D = translatePoint(super.getCenter(), cam);
+		Vector2 center2D = translatePoint(super.getCenter(), cam);
 		g.fillOval(center2D.getX() - radius2D, center2D.getY() - radius2D, 2*radius2D, 2*radius2D);
 	}
 	
 	private int get2DRadius(Camera cam)
 	{
-		System.out.println();
-		Position3D center = super.getCenter();
-		Position3D camPos = cam.getPosition();
+		//System.out.println();
+		Vector3 center = super.getCenter();
+		Vector3 camPos = cam.getPosition();
 		double distance = super.getDistance(cam);
 		double horizDistance = Math.sqrt(
 			Math.pow(center.getZ()-camPos.getZ(), 2) + 
 			Math.pow(center.getX()-camPos.getX(), 2)
 		);
-		System.out.println("Distance:" + distance + " " + horizDistance);
+		//System.out.println("Distance:" + distance + " " + horizDistance);
 		//will need to also adjust by x/z Angle
 		double yAngleSin = (camPos.getY() - center.getY()) / distance;
 		double yAngleCos = horizDistance / distance;
 		double zAngleSin = (camPos.getZ() - center.getZ()) / horizDistance;
 		double zAngleCos = (camPos.getX() - center.getX()) / horizDistance;
-		System.out.println(yAngleSin + " " + yAngleCos + " " + zAngleSin + " " + zAngleCos);
+		//System.out.println(yAngleSin + " " + yAngleCos + " " + zAngleSin + " " + zAngleCos);
 		double height = radius*yAngleCos;
 		double width = radius*yAngleSin;
 		double x = width*zAngleSin;
 		double z = width*zAngleCos;
-		Position3D top = new Position3D(
+		Vector3 top = new Vector3(
 			center.getX() + x, 
 			center.getY() + height,
 			center.getZ() + z
 		);
-		Position3D topRev = new Position3D(
+		Vector3 topRev = new Vector3(
 			center.getX() - x, 
 			center.getY() + height,
 			center.getZ() - z
 		);
-		System.out.println("Center: " + center + ", top: " + top);
+		//System.out.println("Center: " + center + ", top: " + top);
 		
-		Position2D center2D = translatePoint(center, cam);
-		Position2D top2D = translatePoint(top, cam);
-		Position2D topRev2D = translatePoint(topRev, cam);
+		Vector2 center2D = translatePoint(center, cam);
+		Vector2 top2D = translatePoint(top, cam);
+		Vector2 topRev2D = translatePoint(topRev, cam);
 		
 		int result = (int)distanceBetweenPoints(top2D, center2D);
 		int resultRev = (int) distanceBetweenPoints(topRev2D, center2D);
